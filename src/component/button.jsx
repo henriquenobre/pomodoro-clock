9360128c8bbd4e108ef849d20e3665ea0fe3c9a0
Henrique Nobre <henriquenobr@gmail.com>
2021-11-29 21:45:48 -0300

    everything working

--- src/component/button.jsx
+++
@@ -1,9 +0,0 @@
-import react from "react";
-
-const Button = ({tittle, activeClass, _callback}) => {
-    return(
-        <button className={activeClass} onClick={_callback}>{tittle}</button>
-    )
-}
-
-export default Button
--- src/context/SettingsContext.jsx
+++ src/context/SettingsContext.jsx
@@ -1,34 +1,86 @@
-import { createContext, useState } from "react"
+import { useState, createContext } from "react";
 
-export const SettingContext = createContext()
+export const SettingsContext = createContext()
 
-const SettingsContextProvider = (props) => {
+function SettingsContextProvider(props) {
 
-    const [pomodoro, setPomodoro] = useState()
-    const [executing, setExecuting] = useState()
-    const [startAnimation, setStartAnimation] = useState(false)
+    const [pomodoro, setPomodoro] = useState(0)
+    const [executing, setExecuting] = useState({})
+    const [startAnimate, setStartAnimate] = useState(false)
 
+    function setCurrentTimer (active_state) {
+        updateExecute({
+            ...executing,
+            active: active_state
+        })
+        setTimerTime(executing)
+    }
+
+    // start animation fn 
     function startTimer() {
-        setStartAnimation(true)
+        setStartAnimate(true)
     }
-
+    // pause animation fn 
     function pauseTimer() {
-        setStartAnimation(false)
+    setStartAnimate(false)
+    }
+    // pass time to counter 
+    const children = ({ remainingTime }) => {
+    const minutes = Math.floor(remainingTime / 60)
+    const seconds = remainingTime % 60
+    
+    return `${minutes}:${seconds}`
     }
 
-    function stopTimer() {
-        setStartAnimation(false)
+    // clear session storage 
+    const SettingsBtn = () => {
+        setExecuting({})
+        setPomodoro(0)
     }
 
-    const updateExecute = (updateExecute) => {
-        setExecuting(updateExecute)
+    const updateExecute = updatedSettings => {
+        setExecuting(updatedSettings)
+        setTimerTime(updatedSettings)
     }
 
-    return(
-        <SettingContext.Provider value={{stopTimer}}>
+    const setTimerTime = (evaluate) => {
+        switch (evaluate.active) {
+            case 'work':
+                setPomodoro(evaluate.work)
+                break;
+            case 'short':
+                setPomodoro(evaluate.short)
+                break;
+            case 'long':
+                setPomodoro(evaluate.long)
+                break;
+                default:
+                    setPomodoro(0)
+                break;
+        }
+    }
+
+    function stopAimate() {
+        setStartAnimate(false)
+    }
+
+    return (
+        <SettingsContext.Provider value={{
+            pomodoro, 
+            executing,
+            updateExecute, 
+            startAnimate, 
+            startTimer,
+            pauseTimer,
+            children,
+            SettingsBtn,
+            setCurrentTimer,
+            stopAimate
+        }}>
             {props.children}
-        </SettingContext.Provider>
+        </SettingsContext.Provider>
     )
+
 }
 
 export default SettingsContextProvider
